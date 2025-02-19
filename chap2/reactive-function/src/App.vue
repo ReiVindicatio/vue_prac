a4f994a0563f9a2a5a35fe508e68a8d0f54b8b18
斎藤理人 <MasatoSaito@MacBook-Air.local>
2025-02-19 18:02:30 +0900

    a

---
+++ chap2/reactive-function/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {reactive, computed} from "vue";
+
+const data = reactive({
+  PI: 3.14,
+  radius: Math.round(Math.random() * 10)
+});
+const area = computed(
+  (): number => {
+    return data.radius * data.radius * data.PI;
+  }
+);
+setInterval(
+  (): void => {
+    data.radius = Math.round(Math.random() * 10);
+  },
+  1000
+);
+</script>
+
+<template>
+  <p>{{ area }}</p>
+</template>
---
+++ chap4/directive-bind-basic/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const url = ref("https://vuejs.org/")
+</script>
+
+<template>
+  <p><a v-bind:ref="url" target="_blank">Vue.jsのサイト</a></p>
+  <p><a :href="url" target="_blank">Vue.jsのサイト（省略形）</a></p>
+  <p><a v-bind:href="url + 'guide/introduction.html'"></a></p>
+</template>
