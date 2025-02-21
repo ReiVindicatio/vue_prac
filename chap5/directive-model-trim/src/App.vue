661bcca62e64f37b8b52fc013cc00a94205948ce
masato.b.saito <masato.b.saito@accenture.com>
2025-02-21 17:58:16 +0900

    a

---
+++ chap5/directive-model-trim/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const trimmedText = ref("");
+</script>
+
+<template>
+  <section>
+    <input type="text" v-model.trim="trimmedText">
+    <p>にゅうりょくもじれつ：{{ trimmedText }}</p>
+  </section>
+</template>
+
---
+++ chap5/directive-pre/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <section v-pre>
+    <p v-on:click="showHello">{{ hello! }}</p>
+  </section>
+</template>
+
+
