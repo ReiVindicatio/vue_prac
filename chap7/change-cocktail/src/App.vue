bb2dc7dda3711ca6781cf1dc12dce746ff9c3d03
masato.b.saito <masato.b.saito@accenture.com>
2025-04-04 13:29:10 +0900

    a

---
+++ chap7/change-cocktail/src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import {ref, computed} from "vue";
+
+const cocktailDataInit = new Map<number, Cocktail>();
+cocktailDataInit.set(1, {id: 1, name: "white lady", price: 1200});
+cocktailDataInit.set(2, {id: 2, name: "blue hawaii", price: 1500});
+cocktailDataInit.set(3, {id: 3, name: "new york", price: 1100});
+cocktailDataInit.set(4, {id: 4, name: "martini", price: 1500});
+
+const cocktailNo = ref(1);
+const priceMsg = computed(
+  (): string => {
+    const cocktail = cocktailDataInit.get(cocktailNo.value);
+    let msg = "Cocktail not found.";
+    if (cocktail != undefined) {
+      msg = `The price of ${cocktail.name} is ${cocktail.price}.`;
+    }
+    return msg;
+  }
+);
+
+setInterval(
+  (): void => {
+    cocktailNo.value = Math.round(Math.random() * 3) + 1;
+  }
+, 1000);
+
+interface Cocktail {
+  id: number;
+  name: string;
+  price: number;
+};
+</script>
+
+<template>
+  <p>CocktailNo: {{ cocktailNo }}</p>
+  <p>{{ priceMsg }}</p>
+</template>
+
