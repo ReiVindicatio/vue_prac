5f9cca773581d67e5c773783810e7b48b80a7030
斎藤理人 <MasatoSaito@MacBook-Air.local>
2025-04-20 22:29:41 +0900

    a

---
+++ chap9/slot-dynamic/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import OneSection from "./components/OneSection.vue";
+
+const taroProoblemsInit: string[] = ["電話が通じません", "留守です。"];
+const taro = ref("田中太郎");
+const taroProblems = ref(taroProoblemsInit);
+const jiro = ref("鈴木二郎");
+</script>
+
+<template>
+  <section>
+    <OneSection v-bind:name="taro">
+      <ul>
+        <li v-for="problem in taroProblems" v-bind:key="problem">
+          {{ problem }}
+        </li>
+      </ul>
+    </OneSection>
+    <OneSection v-bind:name="jiro"/>
+  </section>
+</template>
+
